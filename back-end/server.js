4286b38078c7b60e079cbd25481d35b66bc800ee
Brianna Mathre <briannabellemathre@gmail.com>
2020-04-09 21:08:54 -0600

    adding backend

---
+++ back-end/server.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+const mongoose = require('mongoose');
+
+// connect to the database
+mongoose.connect('mongodb://localhost:27017/yonana', {
+  useNewUrlParser: true
+});
+
+const multer = require('multer')
+const upload = multer({
+  dest: '../front-end/public/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
+
+const itemSchema = new mongoose.Schema({
+  title: String,
+  creator: String,
+  ingredients: String,
+  directions: String,
+  path: String,
+});
+
+// Create a model for items in the museum.
+const Item = mongoose.model('Item', itemSchema);
+
+
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+  // Just a safety check
+  if (!req.file) {
+    return res.sendStatus(400);
+  }
+  res.send({
+    path: "/images/" + req.file.filename
+  });
+});
+
+app.post('/api/items', async (req, res) => {
+  const item = new Item({
+    title: req.body.title,
+    creator:req.body.creator,
+    ingredients:req.body.ingredients,
+    directions:req.body.directions,
+    path: req.body.path,
+  });
+  try {
+    await item.save();
+    res.send(item);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+// Get a list of all of the items in the museum.
+app.get('/api/items', async (req, res) => {
+  try {
+    let items = await Item.find();
+    res.send(items);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.delete('/api/items/:id', async (req, res) => {
+  try {
+    await Item.deleteOne({_id:req.params.id});
+    let items = await Item.find();
+    res.send(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.put('/api/items/:id', async (req, res) => {
+  try {
+    let item = await Item.findOne({_id:req.params.id});
+    console.log(req.body);
+    item.title = req.body.title;
+    item.creator = req.body.creator;
+    item.ingredients = req.body.ingredients;
+    item.directions=req.body.directions,
+    item.path = req.body.path,
+    await item.save();
+    res.send(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+
+app.listen(3000, () => console.log('Server listening on port 3000!'));
